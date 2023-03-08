99180f4cfc24d9e3c99de4572cbad555d94cf960
Svitlana Yudina <osbka31@gmail.com>
2023-03-08 15:03:01 +0200

    styles to button were added

--- src/components/StartButton/StartButton.tsx
+++ src/components/StartButton/StartButton.tsx
@@ -4,11 +4,16 @@ import './StartButton.scss';
 
 type Props = {
   type: Actions,
+  setOption: React.Dispatch<React.SetStateAction<string>>
 }
 
-export const StartButton: React.FC<Props> = ({ type }) => {
+export const StartButton: React.FC<Props> = ({ type, setOption }) => {
   return (
-    <button type="button" className="button">
+    <button
+      type="button"
+      className="button"
+      onClick={() => setOption(type)}
+    >
       {type}
     </button>
   );
