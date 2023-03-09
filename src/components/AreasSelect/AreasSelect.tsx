fa0235bf196305e1b5eeb42a7a08cf0d3518e530
Svitlana Yudina <osbka31@gmail.com>
2023-03-09 13:46:42 +0200

    context, loader & areaSelect were added

---
+++ src/components/AreasSelect/AreasSelect.tsx
@@ -0,0 +1,64 @@
+/* eslint-disable no-shadow */
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import { getAreas } from '../../api/api';
+import { AreaData } from '../../types/types';
+import { AdressContext } from '../AdressContext';
+import { Loader } from '../Loader';
+import './AreasSelect.scss';
+
+export const AreasSelect: React.FC = () => {
+  const [areas, setAreas] = useState<AreaData[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const { areaValue, setAreaValue } = useContext(AdressContext);
+
+  const loadAreas = useCallback(async() => {
+    setIsLoading(true);
+
+    try {
+      const areasFromServer = await getAreas();
+
+      setAreas(areasFromServer);
+    } catch (err) {
+      throw new Error(`${err}`);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    loadAreas();
+  }, []);
+
+  return (
+    <div className="areaSelect">
+      {/* <Loader /> */}
+      {isLoading && (
+        <Loader />
+      )}
+      {areas.length !== 0 && (
+        <label className="areaSelect__label">
+          <select
+            className="areaSelect__select"
+            name="areas"
+            value={areaValue}
+            onChange={(event) => {
+              setAreaValue(event.target.value);
+            }}
+          >
+            <option value="">Будь ласка, оберіть область</option>
+            {areas.map(area => (
+              <option key={area.Ref} value={area.Ref}>
+                {area.Description}
+              </option>
+            ))}
+          </select>
+        </label>
+      )}
+      {!isLoading && areas.length === 0 && (
+        <p>{'Нажаль не вдалось завантажити дані :('}<br/>
+            Спробуйте, будь ласка, пізніше.
+        </p>
+      )}
+    </div>
+  );
+};
--- src/components/WarehouseList/WarehouseList.tsx
+++ src/components/WarehouseList/WarehouseList.tsx
@@ -1,26 +1,15 @@
-/* eslint-disable no-shadow */
-import React, { useCallback, useEffect, useState } from 'react';
-import { getAreas } from '../../api/api';
-import { AreaData } from '../../types/types';
+import React from 'react';
+import { AdressProvider } from '../AdressContext';
+import { AreasSelect } from '../AreasSelect';
 import './WarehouseList.scss';
 
 export const WarehouseList: React.FC = () => {
-  const [areas, setAreas] = useState<AreaData[]>([]);
-  const [areaValue, setAreaValue] = useState<string>('');
-
-  const loadAreas = useCallback(async() => {
-    const areasFromServer = await getAreas();
-
-    setAreas(areasFromServer);
-  }, []);
-
-  useEffect(() => {
-    loadAreas();
-  }, []);
-
   return (
     <div className="warehouseList">
-      {areas.length !== 0 && (
+      <AdressProvider>
+        <AreasSelect/>
+      </AdressProvider>
+      {/* {areas.length !== 0 && (
         <label className="warehouseList__label">
           <select
             name="areas"
@@ -37,13 +26,7 @@ export const WarehouseList: React.FC = () => {
             ))}
           </select>
         </label>
-      )}
-      {/* <label className="warehouseList__label">
-        <select name="areas">
-          <option value="">Будь ласка, оберіть область</option>
-          <option value=""></option>
-        </select>
-      </label> */}
+      )} */}
     </div>
   );
 }
