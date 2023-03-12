2c9cdbdbaefec24ed11988dca6f004da3bc679d9
Svitlana Yudina <osbka31@gmail.com>
2023-03-12 18:05:23 +0200

    Question was wrapped in react.memo

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -5,13 +5,15 @@ type Props = {
   message: string,
 };
 
-export const Question: React.FC<Props> = ({ message }) => {
-  return (
-    <div className="question">
-      <h1 className="question__title">
-        {message}
-      </h1>
-      <div className="question__image"></div>
-    </div>
-  );
-};
+export const Question: React.FC<Props> = React.memo(
+  function Question({ message }: Props) {
+    return (
+      <div className="question">
+        <h1 className="question__title">
+          {message}
+        </h1>
+        <div className="question__image"></div>
+      </div>
+    );
+  },
+);
