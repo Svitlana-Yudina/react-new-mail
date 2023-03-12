0bab7c61daa3e9fb3a8d6d5d0b4969d1da64dd59
Svitlana Yudina <osbka31@gmail.com>
2023-03-12 17:50:23 +0200

    history was wrapped in react.memo

--- src/components/BrowsingHistory/BrowsingHistory.tsx
+++ src/components/BrowsingHistory/BrowsingHistory.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable no-console */
 /* eslint-disable no-shadow */
-import React from 'react';
+import React, { useCallback } from 'react';
 import '../../additionalStyles/FailMessage.scss';
 import '../../additionalStyles/ContainerStyles.scss';
 import './BrowsingHistory.scss';
@@ -11,46 +12,57 @@ type Props = {
   setIsError: React.Dispatch<React.SetStateAction<boolean>>,
 };
 
-export const BrowsingHistory: React.FC<Props> = ({
-  ttnHistory,
-  setValue,
-  load,
-  setIsError,
-}) => {
-  return (
-    <div className="history list">
-      <div className="history__wraper">
-        <h2 className="history__title">
-          Історія переглядів
-        </h2>
-        {ttnHistory.length === 0 && (
-          <p className="failMessage">
-            Ви поки що не переглянули жодної ТТН
-          </p>
-        )}
-        {ttnHistory.length > 0 && (
-          <>
-            {ttnHistory.map(item => (
-              <a
-                key={item}
-                href="/"
-                className="history__item"
-                onClick={(event) => {
-                  event.preventDefault();
-                  setIsError(false);
-                  setValue(item);
-                  load(item);
-                }}
-              >
-                {item}
-              </a>
-            ))}
-          </>
-        )}
+export const BrowsingHistory: React.FC<Props> = React.memo(
+  function BrowsingHistory({
+    ttnHistory,
+    setValue,
+    load,
+    setIsError,
+  }: Props) {
+    const handleClick = useCallback((
+      event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+      item: string,
+    ) => {
+      event.preventDefault();
+      setIsError(false);
+      setValue(item);
+      load(item);
+    }, []);
+
+    console.log('render');
+
+    return (
+      <div className="history list">
+        <div className="history__wraper">
+          <h2 className="history__title">
+            Історія переглядів
+          </h2>
+          {ttnHistory.length === 0 && (
+            <p className="failMessage">
+              Ви поки що не переглянули жодної ТТН
+            </p>
+          )}
+          {ttnHistory.length > 0 && (
+            <>
+              {ttnHistory.map(item => (
+                <a
+                  key={item}
+                  href="/"
+                  className="history__item"
+                  onClick={(event) => {
+                    handleClick(event, item);
+                  }}
+                >
+                  {item}
+                </a>
+              ))}
+            </>
+          )}
+        </div>
+        <button type="button" className="history__clear">
+          Очистити історію
+        </button>
       </div>
-      <button type="button" className="history__clear">
-        Очистити історію
-      </button>
-    </div>
-  );
-};
+    );
+  },
+);
